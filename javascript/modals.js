0b97ff9d7ffb8533d8f0ed36dc5a70cf30a6fa1b
lortiz24 <luis.ortiz@evius.co>
2023-04-17 16:19:41 -0500

    Se elimina archivo javascrip

--- javascript/modals.js
+++
@@ -1,7 +0,0 @@
-const openModalButton = document.getElementById("openOne");
-openModalButton.addEventListener("click", () => {
-  const modalContent = document.getElementById("modal-one-pice");
-  console.log('modalContent',modalContent)
-  modalContent.classList.add("show");
-  document.body.appendChild(modalContent);
-});
